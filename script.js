18f356ed351438e5d4aa265ad0d7f05f2803c9d9
Larissa Korz <lariskorz@gmail.com>
2023-03-03 16:40:14 -0300

    calculadora

---
+++ script.js
@@ -0,0 +1,24 @@
+const insert = (num) => {
+
+    let numero = document.getElementById("resultado").innerHTML 
+    document.getElementById("resultado").innerHTML = numero + num
+}
+
+const calcular = () => {
+    let resultado = document.getElementById("resultado").innerHTML 
+
+    if(resultado){
+        document.getElementById("resultado").innerHTML = eval(resultado)
+    }else{
+        document.getElementById("resultado").innerHTML = "------"
+    }
+}
+
+const limpar = () => {
+    document.getElementById("resultado").innerHTML = ""
+}
+
+const apagar = () => {
+    let resultado = document.getElementById("resultado").innerHTML 
+    document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1)
+}
